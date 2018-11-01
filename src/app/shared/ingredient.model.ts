a5803af79be1b4dc7b87980521cc8ba541f26dde
marchanda16 <marchandasuk@gmail.com>
2018-11-01 17:26:44 +0700

    first commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient{
+
+        // 1. cara pertama;
+        // public name:string;
+
+        // constructor(name:string,amount:number){
+        //     this.name = name;
+        //     this.number = number;
+        // }
+
+        // 2. cara kedua
+
+    constructor(public name:string, public amount:number){
+
+    }
+}
